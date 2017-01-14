05eeec8abfd864b96ec0a7438d39f79d9fc5a6fc
Ian MacDonald <imacdonald@rumblefish.com>
2017-01-14 13:22:06 -0800

    First commit

---
+++ lib/erb/file_out.rb
@@ -0,0 +1,7 @@
+require "erb/file_out/version"
+
+module Erb
+  module FileOut
+    # Your code goes here...
+  end
+end
---
+++ lib/erb/file_out/version.rb
@@ -0,0 +1,5 @@
+module Erb
+  module FileOut
+    VERSION = "0.1.0"
+  end
+end
